9e330434b2d95aa8b8749cc9f5ed1cce5d9ec244
Hidalgo1996 <GeForce210_1996@hotmail.com>
2023-08-07 00:08:21 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using Foro_proyecto.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<EmpleadosContext>(
+   options => {
+       options.UseSqlServer(builder.Configuration.GetConnectionString("Conexion"));
+   }
+);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// add Cors
+builder.Services.AddCors(options => options.AddPolicy("AllowWebApp",
+    builder => builder.AllowAnyOrigin().
+    AllowAnyHeader().
+    AllowAnyMethod()));
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("AllowWebApp");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
